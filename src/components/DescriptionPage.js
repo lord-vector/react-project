114da5af15afc440b5144a145b26b6a723f43dd6
lord-vector <r.anuj.agrawal@gmail.com>
2023-06-17 19:48:12 +0530

    adding initial style to description

--- src/components/DescriptionPage.js
+++ src/components/DescriptionPage.js
@@ -1,9 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import "./DescriptionPage.css";
 
 export default function DescriptionPage() {
+  const [productDetails, setProductDetails] = useState([]);
+
   const { id } = useParams();
-  console.log(id);
-  return <div>DescriptionPage</div>;
+
+  useEffect(() => {
+    const getProductDetails = async () => {
+      const productsArray = await axios.get(
+        `https://dummyjson.com/products/${id}`
+      );
+      setProductDetails(productsArray.data);
+    };
+    getProductDetails();
+  }, []);
+  console.log(productDetails);
+  return (
+    <div className="description-products">
+      <div className="items">{productDetails.brand}</div>
+      <div className="items">{productDetails.category}</div>
+      <div className="items">{productDetails.description}</div>
+      <div className="items">{productDetails.price}</div>
+      <div className="items">{productDetails.rating}</div>
+      <img className="items img" src={productDetails.thumbnail} />
+    </div>
+  );
 }
